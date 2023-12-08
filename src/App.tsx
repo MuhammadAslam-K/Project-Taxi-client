59d0d1d9a7e2a2ccc36f66d01565ffefde43cbf6
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-12-08 09:42:45 +0530

    Initilaze project structure and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-200 p-4">
+        <h1 className="text-2xl font-bold">Hello, Tailwind CSS in Vite React!</h1>
+        <p className="text-lg mt-2">This is a Vite React app with Tailwind CSS.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
